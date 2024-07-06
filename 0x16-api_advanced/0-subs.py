dddddf3eb0717432b8bb2383719533fbbb27ded8
youness mojahid <mojahidyouness0@gmail.com>
2024-07-06 09:40:05 -0700

    pack

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,23 +7,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    if subreddit is None or not isinstance(subreddit, str):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
-    
-    try:
-        response = requests.get(url, headers=headers, timeout=10)
-        response.raise_for_status()
-        data = response.json()
-        subscribers = data.get("data", {}).get("subscribers", 0)
-        return subscribers
-
-    except requests.RequestException as e:
-        print(f"Request failed: {e}")
-        return 0
-    except ValueError as e:
-        print(f"JSON parsing failed: {e}")
-        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
