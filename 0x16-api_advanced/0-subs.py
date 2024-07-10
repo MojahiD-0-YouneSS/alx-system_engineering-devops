97af28132904ec5b8e22f567273aa1ecb9116137
youness mojahid <mojahidyouness0@gmail.com>
2024-07-10 01:56:54 -0700

    op

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,27 +5,23 @@ Contains the number_of_subscribers function
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
+    print('OK')
     if not subreddit or not isinstance(subreddit, str):
-        print('OK')
         return 0
-    
     headers = {
         'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'
     }
-    
+
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    
+
     try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        response.raise_for_status()
         data = response.json()
-        print('OK')
         return data.get("data", {}).get("subscribers", 0)
-    except requests.exceptions.HTTPError as http_err:
-        print('OK')
-    except requests.exceptions.RequestException as req_err:
-        print('OK')
-    
-    return 0
+    except requests.exceptions.RequestException:
+        return 0
