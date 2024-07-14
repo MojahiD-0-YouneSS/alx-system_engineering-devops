a7a2025492b3270c1456e4748ea214957c00f9a0
youness mojahid <mojahidyouness0@gmail.com>
2024-07-14 02:57:02 -0700

    pwd

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,22 @@
-#!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
 import requests
 
+'''
+ new soloution
+'''
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    print('OK')
-    if not subreddit or not isinstance(subreddit, str):
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
+
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
         return 0
-    headers = {
-        'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'
-    }
 
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        response.raise_for_status()
-        data = response.json()
-        return data.get("data", {}).get("subscribers", 0)
-    except requests.exceptions.RequestException:
-        return 0
+ 
