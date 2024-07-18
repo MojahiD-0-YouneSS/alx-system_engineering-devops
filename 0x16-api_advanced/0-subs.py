da6303d96d74233d722f827d64deb86a01b585f9
youness mojahid <mojahidyouness0@gmail.com>
2024-07-18 05:37:54 -0700

    r

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,19 @@
-#!/usr/bin/python3
 
+#!/usr/bin/python3
 """
-This module provides a function to query the Reddit API and
-retrieve the number of subscribers for a given subreddit.
+Contains the number_of_subscribers function
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers
-    for a given subreddit. If the subreddit is invalid, returns 0.
-
-    Parameters:
-    subreddit (str): The name of the subreddit to query.
-
-    Returns:
-    int: The number of subscribers or 0 if the subreddit is invalid.
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Bot/1.0)'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response:
-        data = response.json()
-        Sub =  data['data'].get('subscribers', 0)
-        return Sub
-    return 0
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                      headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
 
