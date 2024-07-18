baad479a3083bedb34e029d7b05a6327170c5ed1
youness mojahid <mojahidyouness0@gmail.com>
2024-07-18 06:59:57 -0700

    i need a push 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,8 @@
 
 #!/usr/bin/python3
-
+"""
 #Contains the number_of_subscribers function
-
+"""
 
 import requests
 
@@ -11,6 +11,7 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit), headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+    headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
     subs = r.get("data", {}).get("subscribers", 0)
     return subs
