8c18b5ca4ce8bcb8dfb59a919d4ff5d9090c924a
youness mojahid <mojahidyouness0@gmail.com>
2024-07-18 07:16:28 -0700

    i need a push 3

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,8 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "0x16-api_advanced:project:v1.0.0\
+                (by /u/firdaus_cartoon_jr)"
     }
     params = {
         "limit": 10
@@ -16,7 +17,6 @@ def top_ten(subreddit):
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
-        return
+        return 0
     results = response.json().get("data")
     [print(c.get("data").get("title")) for c in results.get("children")]
-
